8ca9b575c020c066ee07f7927e078da40e978f72
vyshnav2003 <puthen1977@gmail.com>
2016-09-28 22:37:10 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/article-one', function (req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('article-two', function (req, res){
+app.get('/article-two', function (req, res){
     res.send('Article one requested and will be served here');
 });
 
