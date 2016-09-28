14e23c9ebda7ab1b73f8400a6e51e928e64de5e2
vyshnav2003 <puthen1977@gmail.com>
2016-09-28 22:40:37 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
